960acaf3a3fde877ec9eed9ff964387a6e1c7343
junchi <zhujunch@msu.edu>
2022-04-23 16:21:17 -0400

    add earth

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React, { useRef, useState, Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Stars } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 import { Physics, usePlane, useBox } from "@react-three/cannon";
 
 import styled from "styled-components";
 
 import "./App.css";
+import {Earth} from "./index";
 
 const CanvasContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: black;
+
 `
 
 function App(){
     return (
         <CanvasContainer>
             <Canvas>
-
-                <OrbitControls />
-                <Stars />
-                <ambientLight intensity={0.5} />
+                <ambientLight intensity={0.2} />
                 <spotLight position={[10, 15, 10]} angle={0.3} />
 
                 <Suspense fallback={null}>
-
+                    <Earth/>
                 </Suspense>
             </Canvas>
     </CanvasContainer>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as THREE from "three";
+import { OrbitControls, Stars } from "@react-three/drei";
+
+import EarthDayMap from "./texture/8k_earth_daymap.jpeg";
+import EarthNormalMap from "./texture/8k_earth_normal_map.jpeg";
+import EarthSpecularMap from "./texture/8k_earth_specular_map.jpeg";
+import EarthCloudsMap from "./texture/8k_earth_clouds.jpeg";
+import { TextureLoader } from "three";
+import {useFrame, useLoader} from "@react-three/fiber";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +24,60 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export function Earth(props){
+    const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(TextureLoader,
+        [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+    );
+
+    const earthRef = useRef();
+    const cloudsRef = useRef();
+
+    useFrame(({ clock }) => {
+        const elapsedTime = clock.getElapsedTime();
+
+        earthRef.current.rotation.y = elapsedTime / 6;
+        cloudsRef.current.rotation.y = elapsedTime / 6;
+    });
+
+    return(
+        <>
+            {}
+        <pointLight color="#f6f3ea" position={[2,0,2]} intensity={3.2} />
+        <Stars
+            radius={300}
+            depth={60}
+            count={20000}
+            factor={7}
+            saturation={0}
+            fade={true}
+        />
+        <mesh ref={cloudsRef} position={[0, 0, 3]}>
+            <sphereGeometry args={[1.005, 32, 32]} />
+            <meshPhongMaterial
+                map={cloudsMap}
+                opacity={0.4}
+                depthWrite={true}
+                transparent={true}
+                side={THREE.DoubleSide}
+            />
+        </mesh>
+        <mesh ref={earthRef} position={[0, 0, 3]}>
+            <sphereGeometry args={[1,32,32]}/>
+            <meshPhongMaterial specularMap={specularMap}/>
+            <meshStandardMaterial map={colorMap}
+                                  normalMap={normalMap}
+                                    metalness={0.4}
+                                    roughness={0.7}/>
+            <OrbitControls
+                enableZoom={true}
+                enablePan={true}
+                enableRotate={true}
+                zoomSpeed={0.6}
+                panSpeed={0.5}
+                rotateSpeed={0.4}
+            />
+        </mesh>
+        </>);
+
+}
