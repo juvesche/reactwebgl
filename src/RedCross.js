cbebb61a61f7876bdd74602799e02967a34e2d2e
junchi <zhujunch@msu.edu>
2022-04-25 23:39:25 -0400

    3d letters and red cross, two objects added

---
+++ src/RedCross.js
@@ -0,0 +1,38 @@
+import {useLoader} from "@react-three/fiber";
+import React, { useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {TextureLoader} from "three";
+import CokeCan from "./texture/red.png";
+
+
+function Cross() {
+    const myMesh = React.useRef();
+    const myMesh2 = React.useRef();
+    const [active, setActive] = useState(false);
+    const sideCan = useLoader(TextureLoader, CokeCan);
+
+
+    useFrame(({ clock }) => {
+        const a = clock.getElapsedTime();
+        myMesh.current.rotation.x = a;
+        myMesh2.current.rotation.x = -a;
+    });
+
+    return (
+        <>
+            <mesh position={[3, 0, 0]}
+                  ref={myMesh}>
+                <boxBufferGeometry attach="geometry" args={[.3, .3, 1, 32,1,true]} />
+                <meshStandardMaterial map={sideCan} />
+            </mesh>
+
+            <mesh position={[3, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}
+                  ref={myMesh2}>
+                <boxBufferGeometry attach="geometry" args={[.3, .3, 1, 32,1,true]} />
+                <meshStandardMaterial map={sideCan} />
+            </mesh>
+        </>
+    );
+}
+export default Cross;
+
