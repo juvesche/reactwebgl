3a9b6d82fcfd7ae17175e643e8170fb4202ede70
junchi <zhujunch@msu.edu>
2022-04-25 23:38:57 -0400

    3d letters and red cross, two objects added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, { useRef, useState, Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from "@react-three/drei";
 import Cylinder from './Cylinder';
+import ReactDOM from 'react-dom'
 import { Physics, usePlane, useBox } from "@react-three/cannon";
+import * as THREE from "three";
+import {Canvas, useFrame} from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { DDSLoader } from "three-stdlib";
 
-
-
+import React, { useRef, useState, Suspense } from 'react'
 import styled from "styled-components";
 
 import "./App.css";
 import {Earth} from "./index";
+import Cross from "./RedCross";
 
 const CanvasContainer = styled.div`
   width: 100vw;
   height: 100vh;
 
 `
+THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
+
+const Scene = () => {
+    const earthRef = useRef();
+    const cloudsRef = useRef();
+
+
+    const materials = useLoader(MTLLoader, "untitled.mtl");
+    const obj = useLoader(OBJLoader, "untitled.obj", (loader) => {
+        materials.preload();
+        loader.setMaterials(materials);
+    });
+
+    console.log(obj);
+    return <primitive object={obj} scale={4.0} position={[1, 1, -40]} rotation={[Math.PI / 2, 0, 0]}/>;
+};
+
 
 function App(){
     return (
@@ -26,8 +47,10 @@ function App(){
                 <spotLight position={[10, 15, 10]} angle={0.3} />
 
                 <Suspense fallback={null}>
+                    <Scene/>
                     <Earth/>
                     <Cylinder/>
+                    <Cross/>
                 </Suspense>
             </Canvas>
     </CanvasContainer>
